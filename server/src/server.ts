a248c85b994dd0ec421ecbe0da75362d47d22733
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-05-16 21:32:10 +0800

    Initialize Playwright test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import surveyRoutes from "../src/routers/surveyRoute"
-import employeesRoute from "../src/routers/employeesRoute"
-import employRoute from "./routers/employRoute"
 import tasksRoute from "./routers/tasksRoute"
 
 dotenv.config();
@@ -12,10 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.use("/api/survey", surveyRoutes)
-app.use("/api/ems", employeesRoute)
-app.use("/api/employees", employRoute)
 app.use("/api", tasksRoute)
 
 const PORT = process.env.PORT || 3002;
